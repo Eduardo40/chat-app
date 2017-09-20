58535f06fbae704e42cfa47fd7d7a4f170ad7712
eduardo <eduardo1@net.hr>
2017-09-21 01:49:14 +0200

    add createMessage and newMessage event

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+const socket = io.connect('http://localhost:3000');
+socket.on('connect', function (data) {
+    socket.on("newUser",function(msg){
+        console.log(msg);
+    })
+    console.log("Connected to the server");
+});
+socket.on("disconnect", function(socekt) {
+    console.log("Bye bye");
+})
+document.querySelector("form").addEventListener("submit",function(e){
+    const to = document.querySelector(".to").value;
+    const text = document.querySelector(".text").value;
+    e.preventDefault()
+    socket.emit("createMessage",{from:to,text:text});
+    document.querySelector(".mesage").innerHTML+=`From: ${to},Text: ${text}<br>`
+//     // window.location.reload();
+})
+socket.on("newMessage",function(msg){
+    console.log("New Message: ",msg);
+    document.querySelector(".mesage").innerHTML+=`From: ${msg.from},Text: ${msg.text} created at: ${msg.createdAt}<br>`
+});
--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 const socket = require("socket.io");
 //port setup
 const port = process.env.PORT || 3000;
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(cors());
+
 //fix ugly path
 const publicPath = path.join(__dirname,"../public");
-
+const msgs = [];
 //setup index.html
 app.use(express.static(publicPath));
 
@@ -21,12 +24,23 @@ app.get("/", (req,res)=>{
 });
 
 io.on('connection', (socket) => {
+    socket.broadcast.emit("newUser","Sombody came here...")
     console.log("User connected");
+    socket.on("newMsg",(msg)=>{
+        console.log(msg.data);
+        msgs.unshift(msg.data);
+    })
+    socket.emit("newMessage",{from:"Eduardo",text:"Hejhejehej"})
     socket.on("disconnect",(socket)=>{
         console.log("Bye bye y cunt");
     })
+    socket.on("createMessage",(msg)=>{
+        console.log(msg);
+        msg.createdAt = new Date().toDateString();
+        // console.log("New email created",msg);
+        socket.broadcast.emit("newMessage",msg)
+    })
   });
-
 
 server.listen(port, () => {
     console.log(`App listening on port ${port}`);

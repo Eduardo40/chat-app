3c56efc4873d4835d02ba9bbd0ea53606a028398
eduardo <eduardo1@net.hr>
2017-09-21 02:18:24 +0200

    bugfix

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const socket = io.connect('https://localhost:3000');
+const socket = io.connect('http://localhost:3000');
 socket.on('connect', function (data) {
     socket.on("newUser",function(msg){
         console.log(msg);
